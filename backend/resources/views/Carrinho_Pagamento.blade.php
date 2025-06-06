78470e7d6e07ea6beb1b3b05f748db87fa1284ce
Pedro Rodrigues <pedro.rodrigues9@aluno.senai.br>
2025-06-06 09:00:46 -0300

    Update Carrinho_Pagamento.blade.php

--- backend/resources/views/Carrinho_Pagamento.blade.php
+++ backend/resources/views/Carrinho_Pagamento.blade.php
@@ -9,7 +9,6 @@
   <link rel="stylesheet" href="/media/Css/Carrinho_Pagamento_Custom.css" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
   <link rel="stylesheet" href="/media/ChatBot/style.css">
-  
 </head>
 <body>
   <header>
