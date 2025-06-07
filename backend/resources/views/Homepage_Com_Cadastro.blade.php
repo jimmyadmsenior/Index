0f54f8afff3f881e5e0abab511468a01cc6a90ad
0TedKaczynski0 <jimmycastilho555@gmail.com>
2025-06-07 17:07:22 -0300

    teste

--- backend/resources/views/Homepage_Com_Cadastro.blade.php
+++ backend/resources/views/Homepage_Com_Cadastro.blade.php
@@ -23,11 +23,11 @@
       </div>
       <nav>
         <ul class="menu">
-          <li><a href="/smartphones">Smartphones</a></li>
-          <li><a href="/tablets">Tablets</a></li>
-          <li><a href="/Homepage_Fones">Fones</a></li>
-          <li><a href="/relogios">Relógios</a></li>
-          <li><a href="/notebooks">Notebooks</a></li>
+          <li><a href="/Smartphone">Smartphones</a></li>
+          <li><a href="/Tablets">Tablets</a></li>
+          <li><a href="/Fones">Fones</a></li>
+          <li><a href="/Relogios">Relógios</a></li>
+          <li><a href="/Notebooks">Notebooks</a></li>
         </ul>
       </nav>
       <div class="icons">
@@ -144,11 +144,11 @@
         </div>
         <div class="footer-section">
             <h4>Recursos</h4>
-            <a href="/smartphones">Smartphones</a>
-            <a href="/tablets">Tablets</a>
-            <a href="/fones">Fones</a>
-            <a href="/relogios">Relógios</a>
-            <a href="/notebooks">Notebooks</a>
+            <a href="/Smartphone">Smartphones</a>
+            <a href="/Tablets">Tablets</a>
+            <a href="/Fones">Fones</a>
+            <a href="/Relogios">Relógios</a>
+            <a href="/Notebooks">Notebooks</a>
         </div>
         <div class="footer-section">
             <h4>Conecte-se</h4>
