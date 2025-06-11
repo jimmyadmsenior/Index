b6c614172abac2ba6444e90968047318f0ac5693
Pedro Rodrigues <173830932+pedrinho0611@users.noreply.github.com>
2025-06-11 12:59:21 -0300

    Update Homepage_Tablet.blade.php

--- backend/resources/views/Homepage_Tablet.blade.php
+++ backend/resources/views/Homepage_Tablet.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title>Fones</title>
   <link rel="icon" type="image/png" href="/media/Ícone_Guia_Navegador_Site.png">
-  <link rel="stylesheet" href="/media/Css/HomePage_Fones.css" />
+  <link rel="stylesheet" href="/media/Css/HomePage_Tablet.css" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
   <style>
     @font-face {
