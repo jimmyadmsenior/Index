991462703882e8437ed33f18b9fa75b2df1b6f44
Jimmy Castilho <jimmycastilho555@gmail.com>
2025-06-11 15:25:12 -0300

    Remove unused views for Notebooks and Relógios, and add Homepage_Tablet.blade.php with complete structure and styling.

--- backend/resources/views/Notebooks.blade.php
+++
@@ -1,9 +0,0 @@
-<link rel="icon" type="image/png" href="/media/Ícone_Guia_Navegador_Site.png">
-<link rel="stylesheet" href="/media/Css/Notebooks.css" />
-<div class="logo">
-        @if(Auth::check())
-            <a href="/Homepage_Com_Cadastro"><img src="/media/Logo_Branca.png" alt="Logo da empresa"></a>
-        @else
-            <a href="/"><img src="/media/Logo_Branca.png" alt="Logo da empresa"></a>
-        @endif
-      </div>
--- backend/resources/views/Relógios.blade.php
+++
@@ -1,9 +0,0 @@
-<link rel="icon" type="image/png" href="/media/Ícone_Guia_Navegador_Site.png">
-<link rel="stylesheet" href="/media/Css/Relógios.css" />
-<div class="logo">
-        @if(Auth::check())
-            <a href="/Homepage_Com_Cadastro"><img src="/media/Logo_Branca.png" alt="Logo da empresa"></a>
-        @else
-            <a href="/"><img src="/media/Logo_Branca.png" alt="Logo da empresa"></a>
-        @endif
-      </div>
